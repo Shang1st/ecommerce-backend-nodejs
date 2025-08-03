ce17e31ae4e86ff7338d969153a9858d35e31250
sangd <shang1st9s@gmail.com>
2025-08-03 12:50:05 +0700

    create new a database shopDEV

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const compression = require('compression')
 const express = require('express')
-const helmet = require('helmet')
+const { default: helmet } = require('helmet')
 const morgan = require('morgan')
 const app = express()
 
@@ -10,14 +10,15 @@ app.use(helmet())
 app.use(compression())
 
 // int db
-
+require('./dbs/init.mongodb')
 
 // int routes
 app.get('/', (req, res, next) => {
     const strCompress = 'Hello Fan!'
     return res.status(200).json({
         message: 'Welcome Fans!',
-        metadata: strCompress.repeat(100000)
+        // metadata: strCompress.repeat(100000)
     })
 })
+
 module.exports = app
